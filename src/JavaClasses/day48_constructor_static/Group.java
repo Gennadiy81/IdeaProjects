3bee94082aa62aafe462d1c3e4a5277314fdcb4a
Gennadiy <ghena187@gmail.com>
2021-05-29 13:24:52 -0500

    third commit

--- src/JavaClasses/day48_constructor_static/Group.java
+++ src/JavaClasses/day48_constructor_static/Group.java
@@ -1,4 +1,45 @@
 package JavaClasses.day48_constructor_static;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+    private String name;
+    private List<String> members = new ArrayList<>();
+
+    public Group(String name){
+      this.name = name;
+    }
+
+    public void addMember(String newMember) {
+    members.add(newMember);
+    }
+
+    public void removeMember(String removeMem){
+    members.remove(removeMem);
+
+    }
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", members=" + members +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
 }

89655ffd191d3645d924cca8a0c818d73fd10e16
Gennadiy <ghena187@gmail.com>
2021-06-01 20:17:47 -0500

    add StaticVariablesTest

--- src/JavaClasses/day49_static/StaticVariables.java
+++ src/JavaClasses/day49_static/StaticVariables.java
@@ -1,5 +1,9 @@
 package JavaClasses.day49_static;
 
 public class StaticVariables {
+     static String name = "Cybertek";
+     static String userName = "admin@cyberrtekschool.com";
+     public static int age = 44;
+     public static double price = 5.99;
 
 }
